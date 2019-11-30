62280eb75ad82a8f2b7771036749537541359f81
Tyler <tpedora@me.com>
2019-11-29 23:21:22 -0700

    feat: populated values working

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const { defaultMargin, defaultMarkup } = require("./defaultValues");
 
 const marginSchema = new Schema({
   key: String,
@@ -11,62 +12,9 @@ const markupSchema = new Schema({
   key: String,
   text: String,
   value: String,
-  markup: Number
+  markup: Number,
+  marginPercent: String
 });
-
-const defaultMargin = [
-  {
-    key: "18",
-    text: "18",
-    value: "18"
-  },
-  {
-  key: "20",
-  text: "20",
-  value: "20"
-  },
-  {
-  key: "22",
-  text: "22",
-  value: "22"
-  },
-  {
-  key: "25",
-  text: "25",
-  value: "25"
-  },
-];
-
-const defaultMarkup = [
-  {
-    key: "21.96",
-    text: "21.96",
-    value: "21.96",
-    markup: 1.135,
-    margin: "18"
-  },
-  {
-    key: "25",
-    text: "25",
-    value: "25",
-    markup: 1.25,
-    margin: "20"
-  },
-  {
-    key: "28.2",
-    text: "28.2",
-    value: "28.2",
-    markup: 1.282,
-    margin: "22"
-  },
-  {
-    key: "33.33",
-    text: "33.33",
-    value: "33.33",
-    markup: 1.333,
-    margin: "25"
-  },
-];
 
 const userSchema = new Schema(
   {
@@ -77,13 +25,12 @@ const userSchema = new Schema(
 );
 
 userSchema.pre('save', function(next) {
-  if (!this._id) {
-    defaultMargin.forEach(function(val) {
-      this.margin.push(val);
-    });
-    defaultMarkup.forEach(function(val) {
-      this.markup.push(val);
-    });
+  for (i = 0; i < defaultMargin.length; i++) {
+    this.margin.push(defaultMargin[i]);
+  };
+
+  for (i = 0; i < defaultMarkup.length; i++) {
+    this.markup.push(defaultMarkup[i]);
   };
 
   next();
---
+++ models/defaultValues.js
@@ -0,0 +1,55 @@
+const defaultMargin = [
+  {
+    key: "18",
+    text: "18",
+    value: "18"
+  },
+  {
+  key: "20",
+  text: "20",
+  value: "20"
+  },
+  {
+  key: "22",
+  text: "22",
+  value: "22"
+  },
+  {
+  key: "25",
+  text: "25",
+  value: "25"
+  },
+];
+
+const defaultMarkup = [
+  {
+    key: "21.96",
+    text: "21.96",
+    value: "21.96",
+    markup: 1.135,
+    marginPercent: "18"
+  },
+  {
+    key: "25",
+    text: "25",
+    value: "25",
+    markup: 1.25,
+    marginPercent: "20"
+  },
+  {
+    key: "28.2",
+    text: "28.2",
+    value: "28.2",
+    markup: 1.282,
+    marginPercent: "22"
+  },
+  {
+    key: "33.33",
+    text: "33.33",
+    value: "33.33",
+    markup: 1.333,
+    marginPercent: "25"
+  },
+];
+
+module.exports = { defaultMargin, defaultMarkup };
