d916a0136bbc885cf4376c1ebc238f07ea016a0b
Tyler <tpedora@me.com>
2019-12-08 13:51:54 -0700

    chore: fixed syntax error, and updated target path

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI = process.env.MONGO_URI,
+  mongoURI: process.env.MONGO_URI,
   googleClientID: process.env.GOOGLE_CLIENTID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   cookieKey: process.env.COOKIE_KEY
