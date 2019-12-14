071e4a95b0ef4672a2f0e76eab0157d12a921a04
tperoda <36551808+tperoda@users.noreply.github.com>
2019-12-14 13:32:44 -0700

    Removing Dev.js to keep keys hidden
    Also setting up Heroku to handle preprod and prod deployments

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb+srv://tpedora:X4SEMsv6pXI6a5NM@cluster0-yciqy.mongodb.net/test?retryWrites=true&w=majority",
-  googleClientID: "856044562046-8aeeq43jr2bmebrg75hbhg5olaffmg79.apps.googleusercontent.com",
-  googleClientSecret: "muTKtFnKP495tDiJD1eSmLxk",
-  cookieKey: "123kjif90asde"
-};
